71f4d1669edef423d44f8c8c8700cf4e9daa5478
Petr Faltus <petrfaltus@petrfaltus.net>
2020-03-04 11:35:43 +0100

    Add mnemonic keys and tooltip for About menu item, insert the horizontal glue to the menu bar

--- src/main/java/cz/petrfaltus/filesfinder/Gui.java
+++ src/main/java/cz/petrfaltus/filesfinder/Gui.java
@@ -1,8 +1,12 @@
 package cz.petrfaltus.filesfinder;
 
+import java.awt.Component;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
+import javax.swing.Box;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -21,9 +25,12 @@ public class Gui extends JFrame {
         }
     }
 
-    private void AboutApplication() {
+    private String AboutApplicationGetTitle() {
         String title = "About the " + this.getTitle();
+        return title;
+    }
 
+    private void AboutApplication() {
         String message = "Author: Petr Faltus © March 2020";
         message += System.lineSeparator();
         message += System.lineSeparator();
@@ -36,19 +43,28 @@ public class Gui extends JFrame {
 
         message += " ";
 
-        JOptionPane.showMessageDialog(this, message, title, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(this, message, AboutApplicationGetTitle(), JOptionPane.INFORMATION_MESSAGE);
     }
 
     private void Menu() {
         MenuItemsListener menuItemsListener = new MenuItemsListener();
 
+        // horizontal menu glue
+        Component horizontalGlue = Box.createHorizontalGlue();
+
+        // Info menu items
         menuItemAbout = new JMenuItem("About");
+        menuItemAbout.setToolTipText(AboutApplicationGetTitle());
+        menuItemAbout.setMnemonic(KeyEvent.VK_A);
         menuItemAbout.addActionListener(menuItemsListener);
 
-        JMenu menuInfo = new JMenu("Info"); 
+        JMenu menuInfo = new JMenu("Info");
+        menuInfo.setMnemonic(KeyEvent.VK_I);
         menuInfo.add(menuItemAbout);
 
+        // final menu bar
         JMenuBar menuBar = new JMenuBar();
+        menuBar.add(horizontalGlue);
         menuBar.add(menuInfo);
 
         this.setJMenuBar(menuBar);
